3bf852feda7f95350f303df1e5b4adc805893991
datngoca <datngoc252k3@gmail.com>
2024-05-08 05:33:22 +0700

    Done project version 1.1

--- MidleWare/Backend/src/index.js
+++ MidleWare/Backend/src/index.js
@@ -3,7 +3,6 @@ import "./database.js";
 import { PORT } from "./config.js";
 import "./libs/initialSetup.js";
 import http from 'http';
-<<<<<<< HEAD
 import { Server } from 'socket.io';
 
 const httpServer = http.createServer(app);
@@ -61,39 +60,4 @@ io.on('connection', (socket) => {
 
 httpServer.listen(PORT, () => {
     console.log("Server on port", PORT);
-=======
-import WebSocket from 'ws';
-
-const server = http.createServer(app);
-const wss = WebSocket.Server(server);
-
-
-// Hàm gửi tin nhắn tới tất cả các client
-function sendRefreshMessage() {
-    wss.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-            client.send('refresh');
-        }       
-    });
-}
-
-// Endpoint để xử lý thay đổi dữ liệu và gửi yêu cầu làm mới trang web tới các client
-app.get('/update-data', function (req, res) {
-    // Xử lý thay đổi dữ liệu ở đây
-    // Sau khi xử lý, gửi yêu cầu làm mới trang web tới tất cả các client
-    sendRefreshMessage();
-    res.send('Data updated');
-});
-
-wss.on('connection', function connection(ws) {
-    console.log('New client connected');        
-
-    ws.on('message', function incoming(message) {
-        console.log('Received message:', message);
-    });
-});
-
-server.listen(PORT, function listening() {
-    console.log('Server started on port', PORT);
->>>>>>> 0296e808d227ada97ddbe7d4fbde7c6ea447636c
 });
